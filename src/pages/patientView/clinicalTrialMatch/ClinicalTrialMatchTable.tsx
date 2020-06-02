16cfd8be293a85f84cc50901737f5591ad61b1c0
lmahl <mahlmeister@DESKTOP-S3GK7DV.localdomain>
2020-06-02 05:23:12 +0200

    fix bug showing 'show more' when there are no more items

--- src/pages/patientView/clinicalTrialMatch/ClinicalTrialMatchTable.tsx
+++ src/pages/patientView/clinicalTrialMatch/ClinicalTrialMatchTable.tsx
@@ -51,7 +51,7 @@ class CollapseList extends React.PureComponent {
     NUM_LIST_ELEMENTS = 3;
 
     getDiplayStyle(str: String[]) {
-        if (str.length < this.NUM_LIST_ELEMENTS) {
+        if (str.length <= this.NUM_LIST_ELEMENTS) {
             return (
                 <div>
                     <ul>{this.asFirstListElement(str)}</ul>
@@ -84,7 +84,7 @@ class CollapseList extends React.PureComponent {
 
     asFirstListElement(str: String[]) {
         var res: String[] = [];
-        if (str.length < this.NUM_LIST_ELEMENTS) {
+        if (str.length <= this.NUM_LIST_ELEMENTS) {
             for (var i = 0; i < str.length; i++) {
                 res.push(str[i]);
             }
@@ -98,7 +98,7 @@ class CollapseList extends React.PureComponent {
 
     asHiddenListElement(str: String[]) {
         var res: String[] = [];
-        if (str.length >= this.NUM_LIST_ELEMENTS) {
+        if (str.length > this.NUM_LIST_ELEMENTS) {
             for (var i = this.NUM_LIST_ELEMENTS; i < str.length; i++) {
                 res.push(str[i]);
             }
